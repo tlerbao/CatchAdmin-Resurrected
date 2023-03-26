6b9f7dfdec1b9550830c2433fa9814b6e2cc9010
tlerbao <lnnw@163.com>
2023-03-26 15:18:11 +0800

    feat: 🚀 增加 Deplyer 自动部署

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Config
+
+set('repository', 'git@github.com:tlerbao/CatchAdmin-Resurrected.git');
+
+set('composer_options', ' --ignore-platform-reqs');
+
+add('shared_files', [
+    '.env'
+]);
+add('shared_dirs', [
+    'runtime',
+    'public/upload',
+    'public/exports',
+]);
+add('writable_dirs', [
+    'runtime/',
+    'public/upload/',
+    'public/exports/'
+]);
+
+// Hosts
+
+host('BeiJing')
+    ->set('hostname', 'TalentServerBeiJing')
+    ->set('deploy_path', '/www/wwwroot/Api.MiaoQiang.top');
+
+// Hooks
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:publish',
+    'deploy:vendors'
+]);
+after('deploy:failed', 'deploy:unlock');
